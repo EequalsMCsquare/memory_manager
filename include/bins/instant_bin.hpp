0ee6f0711a097a3393dbd6f14ad09f622dbde012
EequalsMC2 <zhongxiao0711@gmail.com>
2021-01-04 16:25:41 +0800

    impl instant bin

--- include/bins/instant_bin.hpp
+++ include/bins/instant_bin.hpp
@@ -2,17 +2,44 @@
 
 #include "base_bin.hpp"
 #include <map>
+#include <memory>
 #include <string_view>
+
+#include <shm_kernel/shared_memory.hpp>
 
 namespace libmem {
 class instant_bin : base_bin
 {
 private:
-  std::string_view                  arena_name_;
-  std::map<int, libshm::shm_handle> segments_;
+  std::string_view                                   arena_name_;
+  std::map<int, std::shared_ptr<libshm::shm_handle>> segments_;
 
 public:
   explicit instant_bin(std::atomic_size_t& segment_counter,
                        std::string_view    arena_name);
+
+  instant_bin() = delete;
+
+  instant_bin(const instant_bin&) = delete;
+
+  /**
+   * @brief
+   *
+   * @param nbytes
+   * @return const int64_t segment_id
+   */
+  const int64_t malloc(const size_t& nbytes) noexcept override;
+
+  int free(const size_t& segment_id,
+           const size_t& nbytes = 0) noexcept override;
+
+  void clear() noexcept override;
+
+  const size_t shmhdl_count() noexcept;
+
+  const size_t size() noexcept;
+
+  std::shared_ptr<libshm::shm_handle> get_shmhdl_ptr(
+    const size_t& seg_id) noexcept;
 };
 }
--- src/bins/instant_bin.cxx
+++ src/bins/instant_bin.cxx
@@ -1,4 +1,5 @@
 #include "bins/instant_bin.hpp"
+#include <fmt/format.h>
 
 namespace libmem {
 instant_bin::instant_bin(std::atomic_size_t& segment_counter,
@@ -6,4 +7,56 @@ instant_bin::instant_bin(std::atomic_size_t& segment_counter,
   : base_bin(segment_counter)
   , arena_name_(arena_name)
 {}
+
+const int64_t
+instant_bin::malloc(const size_t& nbytes) noexcept
+{
+  std::lock_guard<std::mutex> GGGGGGGGGGGGGGGGGGGGGGGG(mtx_);
+  size_t                      __tmp = this->segment_counter_ref_++;
+
+  this->segments_.insert(std::make_pair(
+    __tmp,
+    std::make_shared<libshm::shm_handle>(
+      fmt::format("{}#instbin#seg{}", arena_name_, __tmp), nbytes)));
+  return std::move(__tmp);
+}
+
+int
+instant_bin::free(const size_t& segment_id, const size_t& nbytes) noexcept
+{
+  std::lock_guard<std::mutex> GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG(mtx_);
+  auto                        __pair = this->segments_.find(segment_id);
+  if (__pair == this->segments_.end()) {
+    return -1;
+  }
+  this->segments_.erase(__pair);
+  return 0;
+}
+
+void
+instant_bin::clear() noexcept
+{
+  this->segments_.clear();
+}
+
+const size_t
+instant_bin::shmhdl_count() noexcept
+{
+  return this->segments_.size();
+}
+const size_t
+instant_bin::size() noexcept
+{
+  return this->segments_.size();
+}
+
+std::shared_ptr<libshm::shm_handle>
+instant_bin::get_shmhdl_ptr(const size_t& seg_id) noexcept
+{
+  auto __pair = this->segments_.find(seg_id);
+  if (__pair == this->segments_.end()) {
+    return {};
+  }
+  return __pair->second;
+}
 } // namespace libmem
